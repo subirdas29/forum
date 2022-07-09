87115e7261c7bd30279c8c9c3d6fdf152ebf6146
subirdas29 <dasjoy104@gmail.com>
2022-07-09 18:54:40 +0600

    forum all document

---
+++ index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+    #ques {
+        min-height: 500px;
+    }
+    </style>
+    <title>Welcome to EngineeringDiscuss - Forums</title>
+</head>
+
+<body>
+    <?php include 'partials/_header.php';?>
+    <?php include 'partials/_dbconnect.php';?>
+
+    <!--slider start here-->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="Images/coding1.jpg" width=1500px height=500px class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="Images/coding2.jpg" width=1500px height=500px class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="Images/coding3.jpg" width=1500px height=500px class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <!--Category container start here-->
+    <div class="container my-3" id="ques">
+        <h2 class="text-center my-3">EngineeringDiscuss - Catagories </h2>
+        <div class="row">
+
+
+            <!-- Use for loop to iteration through-->
+            <!--fatch all this categories-->
+            <?php 
+         $sql = "SELECT * FROM `catagories`"; 
+         $result = mysqli_query($conn, $sql);
+         while($row = mysqli_fetch_assoc($result)){
+          // echo $row['category_id'];
+          // echo $row['category_name'];
+          $id = $row['category_id'];
+          $cat = $row['category_name'];
+          $desc = $row['category_description'];
+          echo   '<div class="col-md-4 my-2">
+          <div class="card" style="width: 18rem;">
+            
+          <img src="Images/coding' .$id. '.jpg" class="card-img-top" alt="image for this category">
+             
+          <div class="card-body">
+          <h5 class="card-title"><a href="threadlist.php?catid='.$id.'">' . $cat . '</a></h5>
+          <p class="card-text">' . substr($desc, 0, 90). '... </p>
+          <a href="threadlist.php?catid=' . $id . '" class="btn btn-primary">View Threads</a>
+      </div>
+  </div>
+</div>';
+       ;
+         } 
+         ?>
+
+        </div>
+    </div>
+    <?php include 'partials/_footer.php';?>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright EngineeringDiscuss Forums 2022|All rights reserved</p>
+</div>
